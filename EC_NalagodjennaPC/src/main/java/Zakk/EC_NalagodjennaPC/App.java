5446a3a7e09b9723517cbed7f11f78dcd3d2ac5f
ZakkZakk <ZakkZakk@Zakk>
2018-05-18 23:47:21 +0300

    Start

---
+++ EC_NalagodjennaPC/src/main/java/Zakk/EC_NalagodjennaPC/App.java
@@ -0,0 +1,13 @@
+package Zakk.EC_NalagodjennaPC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
